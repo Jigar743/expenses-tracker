12eb874709ee138e2db0346a4a08a902a0c70bad
Jigar743 <76845636+Jigar743@users.noreply.github.com>
2024-08-15 11:35:17 +0530

    updated folder structure

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,15 @@
 import { Inter } from "next/font/google";
+import Head from "next/head";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
-  return <div>Marketing page</div>;
+  return (
+    <>
+      <Head>
+        <title>Expenses Tracker</title>
+      </Head>
+      <div>Marketing page</div>
+    </>
+  );
 }
